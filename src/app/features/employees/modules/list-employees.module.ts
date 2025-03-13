614eb0103565de2c3a2c15b7578d6e16a5d02771
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-13 14:32:03 +0400

    Added list-employee modules

---
+++ src/app/features/employees/modules/list-employees.module.ts
@@ -0,0 +1,11 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+export const ListEmployeesModules = [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule
+];
