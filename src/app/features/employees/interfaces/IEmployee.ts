39a8b411dbd817a86b3c291c7b2bacda7db51893
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-13 14:32:28 +0400

    Added IEmployee interface

---
+++ src/app/features/employees/interfaces/IEmployee.ts
@@ -0,0 +1,11 @@
+export interface EmployeeResponse {
+    id: string;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    email: string;
+    dateJoined: string;
+    department: string;
+    postTitle: string;
+    isActive: string;
+}
