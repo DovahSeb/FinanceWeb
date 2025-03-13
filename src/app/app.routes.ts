5d9cbe5000018102a2ffffb38dc9695942e03fc1
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-13 15:00:00 +0400

    Added list-employees path to route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'employees',
+        loadComponent: () => import('./features/employees/components/list-employees/list-employees.component').then(c => c.ListEmployeesComponent)
+    },
+];
