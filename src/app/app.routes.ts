541e9deeb493a710f5c3a3bca739c258e0ad77ae
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-14 09:12:48 +0400

    Added EditEmployee route to routes config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
         path: 'employees',
         loadComponent: () => import('./features/employees/components/list-employees/list-employees.component').then(c => c.ListEmployeesComponent)
     },
+    {
+        path: 'employee/:id',
+        loadComponent: () => import('./features/employees/components/edit-employee/edit-employee.component').then(c => c.EditEmployeeComponent)
+    },
 ];
