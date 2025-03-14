6b0ef19bc6af9c9d517f624b401e5d107ca6d37a
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-14 10:51:41 +0400

    Added Toastr configuration to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
 import { provideHttpClient } from '@angular/common/http';
 import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 
@@ -11,6 +12,13 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
+    provideToastr({
+      timeOut: 2000,
+      positionClass: 'toast-top-center',
+      preventDuplicates: true,
+      closeButton: true,
+      progressBar: true
+    }),
     provideHttpClient(),
     provideNativeDateAdapter(), {provide: MAT_DATE_LOCALE, useValue: 'en-GB'}
   ]
