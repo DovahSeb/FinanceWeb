54fb3bd020c071bf0d9ca74398a95e23dc9811c8
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-18 09:37:56 +0400

    Added AddEmployee modules

---
+++ src/app/features/employees/modules/add-employee.module.ts
@@ -0,0 +1,17 @@
+import { FormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatSelectModule } from "@angular/material/select";
+
+export const AddEmployeesModule = [
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule
+];
