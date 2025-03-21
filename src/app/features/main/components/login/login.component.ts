c91ec58079dd507d3b70fdbd0c10853b7cb16846
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-21 09:13:18 +0400

    Added Login component with sign in form

---
+++ src/app/features/main/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth/auth.service';
+import { LoginRequest } from '../../../../core/interfaces/auth/IAuth';
+import { LoginModules } from '../../modules/login.module';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [LoginModules],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginRequest: LoginRequest = {
+    email: "",
+    password: ""
+  };
+
+  private authService = inject(AuthService);
+  private toastr = inject(ToastrService);
+
+  login(form: NgForm): void {
+    if(!form.valid){
+      this.toastr.error("Please fill out all required fields.", 'Form Validation Error');
+      return;
+    }
+
+    this.authService.login(this.loginRequest).subscribe(() => {
+      this.toastr.success("Login Successful", "Success");
+    })
+  }
+}
