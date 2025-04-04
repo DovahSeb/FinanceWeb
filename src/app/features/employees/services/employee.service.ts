2c845cb9781c32ba6df8a65c5cf839e23d236f5b
Sebastien Larue <sebastienlar619@gmail.com>
2025-04-04 09:06:21 +0400

    Updated EmployeeService methods

--- src/app/features/employees/services/employee.service.ts
+++ src/app/features/employees/services/employee.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable, Signal, signal, WritableSignal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { EmployeeRequest, EmployeeResponse } from '../interfaces/IEmployee';
 
@@ -9,8 +9,7 @@ import { EmployeeRequest, EmployeeResponse } from '../interfaces/IEmployee';
 })
 export class EmployeeService {
   private http = inject(HttpClient);
-
-  private readonly apiUrl = `${environment.baseUrl}/employees`
+  private readonly apiUrl = `${environment.baseUrl}/employees`;
 
   private readonly _employees: WritableSignal<EmployeeResponse[]> = signal([]);
   readonly employees: Signal<EmployeeResponse[]> = this._employees.asReadonly();
@@ -18,33 +17,44 @@ export class EmployeeService {
   private readonly _selectedEmployee: WritableSignal<EmployeeResponse | null> = signal(null);
   readonly selectedEmployee: Signal<EmployeeResponse | null> = this._selectedEmployee.asReadonly();
 
+  private readonly _loadingEmployees = signal(false);
+  readonly loadingEmployees = this._loadingEmployees.asReadonly();
+
   constructor() { }
 
-  getEmployees(): void {
-    this.http
-      .get<EmployeeResponse[]>(`${this.apiUrl}/GetEmployees`)
-      .pipe(tap(employees => this._employees.set(employees)))
-      .subscribe()
+  getEmployees() {
+    this._loadingEmployees.set(true);
+    return this.http
+    .get<EmployeeResponse[]>(`${this.apiUrl}/GetEmployees`)
+    .pipe(
+      tap(employees => {
+        this._employees.set(employees);
+        this._loadingEmployees.set(false);
+      }),
+    );
   }
 
-  getEmployeeById(id: number): void {
-    this.http
-      .get<EmployeeResponse>(`${this.apiUrl}/GetEmployeeById/${id}`)
-      .pipe(tap(employee => this._selectedEmployee.set(employee)))
-      .subscribe()
-  };
-
-  addEmployee(newEmployee: EmployeeRequest): void {
-    this.http
-      .post<EmployeeResponse>(`${this.apiUrl}/CreateEmployee`, newEmployee)
-      .pipe(tap(addedEmployee => this._employees.update(currentEmployees => [...currentEmployees, addedEmployee])))
-      .subscribe()
+  getEmployeeById(id: number) : Observable<EmployeeResponse> {
+    return this.http
+    .get<EmployeeResponse>(`${this.apiUrl}/GetEmployeeById/${id}`)
+    .pipe(
+      tap(employee => this._selectedEmployee.set(employee))
+    );
   }
 
-  updateEmployee(id: number, updatedEmployee: EmployeeRequest): void {
-    this.http
-      .put<EmployeeResponse>(`${this.apiUrl}/UpdateEmployee/${id}`, updatedEmployee)
-      .pipe(tap(employee => this._employees.update(currentEmployees => currentEmployees.map(e => e.id === employee.id ? employee : e))))
-      .subscribe()
+  addEmployee(newEmployee: EmployeeRequest) : Observable<EmployeeResponse> {
+    return this.http
+    .post<EmployeeResponse>(`${this.apiUrl}/CreateEmployee`, newEmployee)
+    .pipe(
+      tap(addedEmployee => this._employees.update(currentEmployees => [...currentEmployees, addedEmployee]))
+    );
+  }
+
+  updateEmployee(id: number, updatedEmployee: EmployeeRequest): Observable<EmployeeResponse> {
+    return this.http
+    .put<EmployeeResponse>(`${this.apiUrl}/UpdateEmployee/${id}`, updatedEmployee)
+    .pipe(
+      tap(employee => this._employees.update(currentEmployees => currentEmployees.map(e => e.id === employee.id ? employee : e))),
+    );
   }
 }
