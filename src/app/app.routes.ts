758258f552690dd9147f4b1dcc7ee4fd9329b3ea
Sebastien Larue <sebastienlar619@gmail.com>
2025-04-09 11:46:06 +0400

    Added Home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./features/main/components/home/home.component').then(c => c.HomeComponent)
+    },
+    {
         path: 'employees',
         loadComponent: () => import('./features/employees/components/list-employees/list-employees.component').then(c => c.ListEmployeesComponent)
     },
